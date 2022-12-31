d86b0f5ffabc46649a4c3bd17cc3aba0d26dbed7
Ulises Viña <contacto@ulisesvina.me>
2022-12-31 06:20:12 -0600

    Added a title.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document: FC = () => (
     <Html>
         <Head>
+            <title>Educaid</title>
+            <meta name="description" content="Educaid" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" type="image/png" href="/Educaid.png" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
         </Head>
         <body>
             <Main />
